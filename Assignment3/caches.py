068d08c8a976df623ba55d3a2a2790dcd559840a
Andreandhiki <andhiki28@gmail.com>
2024-09-13 08:38:47 +0700

    feat[Assignment 3]: Add several materials and code of 3rd assignment

---
+++ Assignment3/caches.py
@@ -0,0 +1,87 @@
+import m5
+from m5.objects import Cache
+
+class L1Cache(Cache):
+    """Simple L1 Cache with default values"""
+
+    assoc = 2
+    tag_latency = 2
+    data_latency = 2
+    response_latency = 2
+    mshrs = 4
+    tgts_per_mshr = 20
+
+    def _init_(self, options=None):
+        super()._init_()
+        pass
+
+    def connectBus(self, bus):
+        """Connect this cache to a memory-side bus"""
+        self.mem_side = bus.cpu_side_ports
+
+    def connectCPU(self, cpu):
+        """Connect this cache's port to a CPU-side port
+        This must be defined in a subclass"""
+        raise NotImplementedError
+
+
+class L1ICache(L1Cache):
+    """Simple L1 instruction cache with default values"""
+
+    # Set the default size
+    size = "16kB"
+
+
+    def _init_(self, opts=None):
+        super()._init_(opts)
+        if not opts or not opts['l1i_size']:
+            return
+        self.size = opts['l1i_size']
+
+    def connectCPU(self, cpu):
+        """Connect this cache's port to a CPU icache port"""
+        self.cpu_side = cpu.icache_port
+
+
+class L1DCache(L1Cache):
+    """Simple L1 data cache with default values"""
+
+    # Set the default size
+    size = "64kB"
+
+
+    def _init_(self, opts=None):
+        super()._init_(opts)
+        if not opts or not opts['l1d_size']:
+            return
+        self.size = opts['l1d_size']
+
+    def connectCPU(self, cpu):
+        """Connect this cache's port to a CPU dcache port"""
+        self.cpu_side = cpu.dcache_port
+
+
+class L2Cache(Cache):
+    """Simple L2 Cache with default values"""
+
+    # Default parameters
+    size = "256kB"
+    assoc = 8
+    tag_latency = 20
+    data_latency = 20
+    response_latency = 20
+    mshrs = 20
+    tgts_per_mshr = 12
+
+
+    def _init_(self, opts=None):
+        super()._init_()
+        if not opts or not opts['l2_size']:
+            return
+        self.size = opts['l2_size']
+
+    def connectCPUSideBus(self, bus):
+        self.cpu_side = bus.mem_side_ports
+
+    def connectMemSideBus(self, bus):
+        self.mem_side = bus.cpu_side_ports
